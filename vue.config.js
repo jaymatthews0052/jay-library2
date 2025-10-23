02ec6474014d8a3bddbad8f3895895798bedd09f
JayMatthews9 <jmat0052@student.monash.edu>
2025-10-23 18:35:15 +1100

    Again, trying to fix pages

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  // Must match your repo name exactly
-  base: '/jay-library2/',
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/jay-library2/'
+    : '/'
+}
