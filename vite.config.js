40148e065efc928bfdfce3c1f49abbb18e0126cd
JayMatthews9 <jmat0052@student.monash.edu>
2025-10-23 18:36:35 +1100

    undoing vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  // Must match your repo name exactly
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   base: '/jay-library2/',
 })
